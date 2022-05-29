79c91f88611a815ffb2f88ef358a8520a9a06275
eunsukimme <eunsu.dev@gmail.com>
2022-05-29 19:11:40 +0900

    feat: add member section

--- src/_pages/index/components/MemberSection.tsx
+++ src/_pages/index/components/MemberSection.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import QuoteCard from './QuoteCard';
+import ArrowRightIcon from 'public/icons/arrow_right.svg';
 
 type Props = {};
 
@@ -11,9 +12,17 @@ export default function MemberSection({}: Props) {
         전문가가 될 수 있다!
       </h1>
 
-      <section>
+      <section className="mt-[40px] flex flex-col gap-y-[24px]">
+        <QuoteCard />
         <QuoteCard />
       </section>
+
+      <div className="flex justify-center">
+        <button className="mt-[60px] flex items-center gap-x-[8px] rounded-[20px] bg-primary py-[6px] px-[48px] text-[14px] font-bold leading-[36px] text-white">
+          AUSG 멤버 더 살펴보기
+          <ArrowRightIcon className="text-white" />
+        </button>
+      </div>
     </div>
   );
 }
--- src/_pages/index/components/QuoteCard.tsx
+++ src/_pages/index/components/QuoteCard.tsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import QuoteIcon from 'public/icons/quote.svg';
 
 type Props = {};
 
 export default function QuoteCard({}: Props) {
   return (
-    <div className="bg-gray-100 px-[30px] pt-[40px] pb-[20px]">
-      <p className="text-[64px] font-bold leading-[22px] text-primary">"</p>
+    <div className="rounded-[20px] bg-gray-100 px-[30px] pt-[40px] pb-[20px]">
+      <QuoteIcon className="text-primary" />
+      <div className="leading-22px mt-[12px]">
+        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet
+        sint. Velit officia consequat duis enim velit mollit. Exercitation
+        veniam consequat sunt nostrud amet.
+      </div>
+      <div className="mt-[26px] flex items-center">
+        <img
+          src="images/profile-1.png"
+          className="mr-[8px] h-[48px] w-[48px] shrink-0"
+        />
+        <div className="text-[14px] leading-[18px]">
+          <strong>현 마켓컬리 엔지니어 김민태</strong>
+          <br />
+          <span className="text-[12px]">AUSG 4기</span>
+        </div>
+      </div>
     </div>
   );
 }
