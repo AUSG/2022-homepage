ae3f78b0ddb3c3de0b2b86e522da8d0c371ec8f1
naru200 <jinsu2504@gmail.com>
2022-06-07 00:28:58 +0900

    Implement Contact page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,19 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import type { AppProps } from 'next/app';
 
 function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
 export default App;
