4091643f9c52b68fb4ad116ead2bbadc0b9933a1
eunsukimme <eunsu.dev@gmail.com>
2022-06-07 01:17:43 +0900

    feat: add og tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css';
 import Head from 'next/head';
 import type { AppProps } from 'next/app';
+import { DefaultSeo } from 'next-seo';
 
 function App({ Component, pageProps }: AppProps) {
   return (
@@ -11,6 +12,26 @@ function App({ Component, pageProps }: AppProps) {
           content="width=device-width, initial-scale=1, maximum-scale=1"
         />
       </Head>
+      <DefaultSeo
+        title="AUSG"
+        description="압도적 쓱케일, 아우쓱 - AWSKRUG University Student Group"
+        openGraph={{
+          url: 'https://ausg.me',
+          title: 'AUSG',
+          description:
+            '압도적 쓱케일, 아우쓱 - AWSKRUG University Student Group',
+          images: [
+            {
+              url: '/images/og.png',
+              width: 1200,
+              height: 600,
+              alt: 'Og Image Alt',
+              type: 'image/jpeg',
+            },
+          ],
+          site_name: 'AUSG Homepage',
+        }}
+      />
       <Component {...pageProps} />
     </>
   );
