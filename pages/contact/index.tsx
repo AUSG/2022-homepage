c330f8a8a91b53fe6df0315af74abf8d0c79f8e0
naru200 <jinsu2504@gmail.com>
2022-08-19 04:29:06 +0900

    Add Spinner to contact page

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -40,14 +40,35 @@ const Contact = () => {
           </div>
         </div>
 
-        <div className="relative mx-5 my-6 md:my-10">
+        <div className="relative mx-5 mt-6 mb-12 md:mt-10">
           {isLoaded ? undefined : (
-            <div className="absolute inset-0 h-[440px] w-full bg-gray-100" />
+            <div className="absolute inset-0 flex h-[440px] w-full items-center justify-center bg-gray-100">
+              <svg
+                className="h-8 w-8 animate-spin text-gray-900"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <circle
+                  className="opacity-25"
+                  cx="12"
+                  cy="12"
+                  r="10"
+                  stroke="currentColor"
+                  strokeWidth="4"
+                />
+                <path
+                  className="opacity-75"
+                  fill="currentColor"
+                  d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+                />
+              </svg>
+            </div>
           )}
           <iframe
             src="https://tally.so/embed/w447O5?alignLeft=1&hideTitle=1&transparentBackground=1"
             width="100%"
-            height="440"
+            height="480"
             title="AUSG로 문의하기"
             className="z-10"
             onLoad={() => setIsLoaded(true)}
