b0b3f2bfad52b010920c50d4c13ae364847ac072
naru200 <jinsu2504@gmail.com>
2022-09-04 03:14:12 +0900

    Randomly shuffle the IntroCard on the People page

--- pages/people/index.tsx
+++ pages/people/index.tsx
@@ -1,3 +1,4 @@
+import { Fragment } from 'react';
 import Head from 'next/head';
 
 import Header from '@/components/Header';
@@ -6,9 +7,26 @@ import IntroCard from '@/_pages/people/IntroCard';
 
 import peopleData from 'data/people.json';
 
+const shuffle = (array: any[], randomNumber: number) => {
+  let currentIndex = array.length;
+  let randomIndex;
+
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(randomNumber * currentIndex);
+    currentIndex -= 1;
+
+    // eslint-disable-next-line no-param-reassign
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex],
+      array[currentIndex],
+    ];
+  }
+
+  return array;
+};
 const TAB = ['6th', '5th', '4th', '3rd', '2nd', '1st'];
 
-const People = () => {
+const People = ({ randomNumber }: { randomNumber: number }) => {
   return (
     <>
       <Head>
@@ -23,21 +41,28 @@ const People = () => {
         {TAB.map(
           tab =>
             peopleData.people.filter(({ year }) => year === tab).length > 0 && (
-              <>
-                <ScrollNavigation key={`tab_${tab}`} currentTab={tab} />
+              <Fragment key={tab}>
+                <ScrollNavigation currentTab={tab} />
                 <div className="mt-8 mb-6 grid grid-cols-1 gap-y-4 px-5 lg:mt-20 lg:mb-[60px] lg:grid-cols-2 lg:gap-x-16 lg:gap-y-12 lg:px-8">
-                  {peopleData.people
-                    .filter(({ year }) => year === tab)
-                    .map(person => (
-                      <IntroCard key={person.name_en} {...person} />
-                    ))}
+                  {shuffle(
+                    peopleData.people.filter(({ year }) => year === tab),
+                    randomNumber
+                  ).map(person => (
+                    <IntroCard key={person.name_en} {...person} />
+                  ))}
                 </div>
-              </>
+              </Fragment>
             )
         )}
       </div>
     </>
   );
 };
+
+// 왜 굳이 랜덤 넘버를 서버에서 생성해서 props로 넘겨주는가?
+// => 이렇게 안하면 Server에서 랜덤으로 배치한 UI와 Client에서 랜덤으로 배치한 UI가 서로 달라서 hydration이 실패하기 때문
+export const getServerSideProps = async () => ({
+  props: { randomNumber: Math.random() },
+});
 
 export default People;
