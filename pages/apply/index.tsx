c2f277752e350aad06eb3b10b9e4cc80d5a3e082
김태강 <k_2684@naver.com>
2024-06-14 22:13:33 +0900

    Add AUSG 8th recruitment application page and dayjs

--- pages/apply/index.tsx
+++ pages/apply/index.tsx
@@ -1,6 +1,32 @@
 import Head from 'next/head';
 
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
 const Index = () => {
+  const targetDate = dayjs('2024-06-27 23:59:59').tz('Asia/Seoul').toDate(); // 8기 지원 마감일 (KST)
+
+  const krCurrentDate = dayjs().tz('Asia/Seoul').toDate(); // 한국 시간 기준 현재 시간
+
+  if (krCurrentDate > targetDate) {
+    return (
+      <>
+        <Head>
+          <title>AUSG 8기 지원하기</title>
+        </Head>
+        <div className="flex h-screen w-screen items-center justify-center">
+          <h1 className="text-center text-4xl font-bold text-primary">
+            8기 지원이 마감되었습니다.
+          </h1>
+        </div>
+      </>
+    );
+  }
+
   return (
     <>
       <Head>
