80e636126fa7c15b9ac9b39d366df5660597ac8d
김태강 <k_2684@naver.com>
2024-06-14 22:17:52 +0900

    Implement Countdown hook

---
+++ src/_pages/hooks/useCountdown.tsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from 'react';
+
+export default function useCountdown(targetDate: Date): Number[] {
+  const getReturnValues = (countDown: number) => {
+    // calculate time left
+    const days = Math.floor(countDown / (1000 * 60 * 60 * 24));
+    const hours = Math.floor(
+      (countDown % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    const minutes = Math.floor((countDown % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((countDown % (1000 * 60)) / 1000);
+
+    return [days, hours, minutes, seconds];
+  };
+
+  const countDownDate = new Date(targetDate).getTime();
+
+  const [countDown, setCountDown] = useState(
+    countDownDate - new Date().getTime()
+  );
+
+  useEffect(() => {
+    setInterval(() => {
+      setCountDown(countDownDate - new Date().getTime());
+    }, 1000);
+  }, [countDownDate]);
+
+  return getReturnValues(countDown);
+}
