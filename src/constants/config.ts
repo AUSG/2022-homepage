4744e948407ca0a5a9b861e85293615ef62e269d
Dohun Kim <52370750+kdhhuns2000@users.noreply.github.com>
2025-06-13 01:24:43 +0900

    fix: Change tallyFormURL (#160)

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -10,7 +10,7 @@ export const RECRUITMENT_CONFIG = {
   generation: 9, // 기수
   openDate: dayjs('2025-06-13 00:00:00').tz('Asia/Seoul'), // 모집 시작일
   deadlineDate: dayjs('2025-06-30 23:59:59').tz('Asia/Seoul'), // 모집 마감일
-  tallyFormUrl: 'https://tally.so/r/mVBXGN', // Tally 폼 URL
+  tallyFormUrl: 'https://tally.so/r/nrbQ2N', // Tally 폼 URL
   emailApiUrl:
     'https://ovshxcxfyslspeeqa26og2uvya0gqkfc.lambda-url.ap-northeast-2.on.aws/email/register', // 이메일 등록 API URL
   showEmailInput: true, // 모집 알림 이메일 여부
