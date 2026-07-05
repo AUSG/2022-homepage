b2dbead45bb3f3b9ae4883519d37c0d953c25638
gun9212 <gun9212@naver.com>
2026-07-05 21:04:48 +0900

    fix: 10기 리크루팅 마감으로 인한 홈페이지 변경

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -7,12 +7,12 @@ dayjs.extend(timezone);
 
 // 모집 설정
 export const RECRUITMENT_CONFIG = {
-  generation: 10, // 기수
+  generation: 11, // 기수
   openDate: dayjs('2026-06-01 20:00:00').tz('Asia/Seoul'), // 모집 시작일
   deadlineDate: dayjs('2026-06-30 23:59:59').tz('Asia/Seoul'), // 모집 마감일
   tallyFormUrl: 'https://tally.so/r/5Beolv', // Tally 폼 URL
   emailApiUrl:
     'https://ovshxcxfyslspeeqa26og2uvya0gqkfc.lambda-url.ap-northeast-2.on.aws/email/register', // 이메일 등록 API URL
   showEmailInput: true, // 모집 알림 이메일 여부
-  showScheduleBubble: true, // 모집 일정 말풍선 표시 여부
+  showScheduleBubble: false, // 모집 일정 말풍선 표시 여부
 };
